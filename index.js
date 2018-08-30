dcbda8b1e0dc41c834441755ca9ac50942dc7408
In Seop Hwang <stevehwang4147@gmail.com>
2018-08-30 00:50:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Steve"
-var height = "69"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
-console.log(parseInt(height) +1 )
+console.log(height +1 )
 console.log(height)
